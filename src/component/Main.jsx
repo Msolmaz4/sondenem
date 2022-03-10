a5bb17957c532c1ef518d9fd74c85c50c7bd44ba
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-03-10 12:19:46 +0100

    freibaseiaufh;ren

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+
+import React, {  useState } from 'react'
+import Input from './Input'
+import { data } from '../data'
+import PostCard from './PostCard'
 
 const Main = () => {
+
+const [datal,setDatal] = useState(data)
+
+
+  
   return (
-    <div>Main</div>
+    <>
+    <Input/>
+    <div>
+
+    <div>
+{
+  datal.map((e)=>(
+    <PostCard
+    ImgUrl={e.image}
+    title={e.title}
+    rating={e.rating}
+    price={e.price}
+    id={e.product_id}
+    />
+
+  ))
+ 
+}
+</div>
+
+
+    </div>
+    
+    
+    
+    
+    
+    </>
   )
 }
 
--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { auth } from '../auth/fire-base'
+import { createUserWithEmailAndPassword} from 'firebase/auth'
 
 const Login = () => {
 
