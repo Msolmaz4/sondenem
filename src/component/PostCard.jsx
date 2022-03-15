7d09376e49b9306012d581fa4a52563f37c03f8f
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-03-16 00:31:47 +0100

    fertig

--- src/component/PostCard.jsx
+++ src/component/PostCard.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import AuthContext from '../context/AuthContext';
+
 
  const PostCard = ({ImgUrl,title,rating,id,price}) => {
+
+  //console.log('calisti')
+  const [fav ,setFav] = useState(false)
+
+  const {favs,addFav,deleteFav} =useContext(AuthContext)
+  //console.log('favs.......',favs)
+
+
+  useEffect(()=>{
+
+    if (fav.length>0){  
+      const filterdeValues = favs.filter(fav =>fav.data.product_id == id)
+      console.log(filterdeValues,'filterded')
+      filterdeValues.length ? setFav(true) : setFav(false)
+    }
+  
+
+
+
+  },[fav])
+
+
+
+
+
+
   return (
+  
+
+
+
+
+
     <div>
 
 
@@ -10,7 +44,43 @@ import React from 'react'
     <h5 className="card-title">{title}</h5>
     <img src={ImgUrl}  className='img' alt='movies'/>
     <p className='price'>{price}$</p>
+    <svg className='sepet' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket3" viewBox="0 0 16 16">
+  <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM3.394 15l-1.48-6h-.97l1.525 6.426a.75.75 0 0 0 .729.574h9.606a.75.75 0 0 0 .73-.574L15.056 9h-.972l-1.479 6h-9.21z"/>
+</svg>
 
+  </div>
+  <div className='ball' onClick={()=>{
+
+    fav ? deleteFav(id) : addFav(id)
+setFav(!fav)
+
+  }}>
+
+
+{
+
+  fav ?(
+
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-balloon-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8.48 10.901C11.211 10.227 13 7.837 13 5A5 5 0 0 0 3 5c0 2.837 1.789 5.227 4.52 5.901l-.244.487a.25.25 0 1 0 .448.224l.04-.08c.009.17.024.315.051.45.068.344.208.622.448 1.102l.013.028c.212.422.182.85.05 1.246-.135.402-.366.751-.534 1.003a.25.25 0 0 0 .416.278l.004-.007c.166-.248.431-.646.588-1.115.16-.479.212-1.051-.076-1.629-.258-.515-.365-.732-.419-1.004a2.376 2.376 0 0 1-.037-.289l.008.017a.25.25 0 1 0 .448-.224l-.244-.487ZM4.352 3.356a4.004 4.004 0 0 1 3.15-2.325C7.774.997 8 1.224 8 1.5c0 .276-.226.496-.498.542-.95.162-1.749.78-2.173 1.617a.595.595 0 0 1-.52.341c-.346 0-.599-.329-.457-.644Z"/>
+</svg>
+
+
+
+    
+  ):
+  (
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-balloon" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M8 9.984C10.403 9.506 12 7.48 12 5a4 4 0 0 0-8 0c0 2.48 1.597 4.506 4 4.984ZM13 5c0 2.837-1.789 5.227-4.52 5.901l.244.487a.25.25 0 1 1-.448.224l-.008-.017c.008.11.02.202.037.29.054.27.161.488.419 1.003.288.578.235 1.15.076 1.629-.157.469-.422.867-.588 1.115l-.004.007a.25.25 0 1 1-.416-.278c.168-.252.4-.6.533-1.003.133-.396.163-.824-.049-1.246l-.013-.028c-.24-.48-.38-.758-.448-1.102a3.177 3.177 0 0 1-.052-.45l-.04.08a.25.25 0 1 1-.447-.224l.244-.487C4.789 10.227 3 7.837 3 5a5 5 0 0 1 10 0Zm-6.938-.495a2.003 2.003 0 0 1 1.443-1.443C7.773 2.994 8 2.776 8 2.5c0-.276-.226-.504-.498-.459a3.003 3.003 0 0 0-2.46 2.461c-.046.272.182.498.458.498s.494-.227.562-.495Z"/>
+  </svg>
+  )
+}
+
+
+
+
+
+ 
   </div>
 </div>
 
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,7 +1,8 @@
 import { onAuthStateChanged } from 'firebase/auth';
 import React ,{createContext, useEffect, useState} from 'react'
 import { auth ,db} from '../auth/fire-base';
-import { collection ,getDocs} from 'firebase/firestore';
+import { addDoc, collection ,getDocs,deleteDoc, doc } from 'firebase/firestore';
+import { data } from '../data';
 
 
 const AuthContext = createContext();
@@ -27,6 +28,36 @@ const getFav = async ()=>{
         setFavs(favs)
 }
 
+const addFav =(id)=>{
+
+    const dataToAdd = data.filter(el=>el.product_id == id)
+    console.log(dataToAdd,'qqqq')
+    const colRef = collection(db,'favs')
+
+
+    
+    addDoc(colRef,{data:dataToAdd[0]})
+    .then(doc=>{
+        //getFavs
+        favs.push({data:dataToAdd[0],id:doc.id})
+        setFavs(favs)
+    })
+}
+
+
+const deleteFav=(id)=>{
+    console.log('delete')
+    const dataToDelete = favs.filter(el=>el.data.product_id == id)
+    const docRef = doc(db,'favs',dataToDelete[0].id)
+    deleteDoc(docRef)
+    .then(()=>{
+        let newfilter = favs.filter(el=>el.data.product_id != id)
+        setFavs(newfilter)
+    })
+
+}
+
+
     useEffect(()=>{
 
         getFav()
@@ -39,7 +70,9 @@ const getFav = async ()=>{
 
 return (
     <AuthContext.Provider value={{currentUser,
-        favs
+        favs,
+        addFav,
+        deleteFav
     
     
     }}>
